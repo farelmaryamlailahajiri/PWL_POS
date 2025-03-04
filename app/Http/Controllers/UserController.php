4298c3573aa96da977e18db2ce6089d76cd650dd
farelmaryamlailahajiri <farelhajiri295@gmail.com>
2025-03-04 11:00:58 +0700

    Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -32,15 +32,28 @@ class UserController extends Controller
         // ];
         // UserModel::create($data);
 
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
         //coba akses model usermodel
-        $user = UserModel::all(); //ambil semua data dari tabel m_user
+        // $user = UserModel::all(); //ambil semua data dari tabel m_user
+        // $user = UserModel::find(1); //ambil data user dengan id 1
+        // $user = UserModel::where('level_id', 1)->first(); // Mengambil satu data pertama dari tabel UserModel yang memiliki level_id = 1
+        // $user = UserModel::firstWhere('level_id', 1); // Mengambil satu data pertama dari tabel UserModel yang memiliki level_id = 1 (sama seperti where()->first())
+
+        // Mencari data UserModel dengan id = 1, hanya mengambil kolom 'username' dan 'nama'.
+        // Jika data tidak ditemukan, jalankan fungsi abort(404) untuk menampilkan error 404
+        // $user = UserModel::findOr(1, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+
+        $user = UserModel::findOr(20, ['username', 'nama'], function(){
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
