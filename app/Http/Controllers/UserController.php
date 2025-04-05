ca790d5f9a2f83486fe6e159235764979660cf14
farelmaryamlailahajiri <farelhajiri295@gmail.com>
2025-04-05 22:01:30 +0700

    filtering&searching jb5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -152,7 +152,7 @@ class UserController extends Controller
 
         // $user = UserModel::all();
         // return view('user', ['data' => $user]);
-        
+
         $breadcrumb = (object) [
             'title' => 'Daftar User',
             'list'  => ['Home', 'User']
@@ -164,7 +164,9 @@ class UserController extends Controller
 
         $activeMenu = 'user'; // set menu yang sedang aktif
 
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+        $level = LevelModel::all(); //ambil data level untuk filter level
+
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
 
     // Ambil data user dalam bentuk json untuk datatables
@@ -173,13 +175,18 @@ class UserController extends Controller
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
             ->with('level');
 
+        // Filter data user berdasarkan level_id
+        if ($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
+
         return DataTables::of($users)
             // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
             ->addIndexColumn()
             ->addColumn('aksi', function ($user) { // menambahkan kolom aksi
-                $btn = '<a href="'.url('/user/' . $user->user_id).'" class="btn btn-info btn-sm">Detail</a> ';
-                $btn .= '<a href="'.url('/user/' . $user->user_id . '/edit').'" class="btn btn-warning btn-sm">Edit</a> ';
-                $btn .= '<form class="d-inline-block" method="POST" action="'.url('/user/' . $user->user_id).'">'
+                $btn = '<a href="' . url('/user/' . $user->user_id) . '" class="btn btn-info btn-sm">Detail</a> ';
+                $btn .= '<a href="' . url('/user/' . $user->user_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
+                $btn .= '<form class="d-inline-block" method="POST" action="' . url('/user/' . $user->user_id) . '">'
                     . csrf_field() . method_field('DELETE') .
                     '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button></form>';
                 return $btn;
@@ -204,9 +211,9 @@ class UserController extends Controller
         $activeMenu = 'user';       // set menu yang sedang aktif
 
         return view('user.create', [
-            'breadcrumb' => $breadcrumb, 
-            'page' => $page, 
-            'level' => $level, 
+            'breadcrumb' => $breadcrumb,
+            'page' => $page,
+            'level' => $level,
             'activeMenu' => $activeMenu
         ]);
     }
@@ -251,9 +258,9 @@ class UserController extends Controller
         $activeMenu = 'user'; // set menu yang sedang aktif
 
         return view('user.show', [
-            'breadcrumb' => $breadcrumb, 
-            'page' => $page, 
-            'user' => $user, 
+            'breadcrumb' => $breadcrumb,
+            'page' => $page,
+            'user' => $user,
             'activeMenu' => $activeMenu
         ]);
     }
@@ -276,10 +283,10 @@ class UserController extends Controller
         $activeMenu = 'user'; // set menu yang sedang aktif
 
         return view('user.edit', [
-            'breadcrumb' => $breadcrumb, 
-            'page' => $page, 
-            'user' => $user, 
-            'level' => $level, 
+            'breadcrumb' => $breadcrumb,
+            'page' => $page,
+            'user' => $user,
+            'level' => $level,
             'activeMenu' => $activeMenu
         ]);
     }
@@ -290,7 +297,7 @@ class UserController extends Controller
         $request->validate([
             // username harus diisi, string, minimal 3 karakter,
             // dan bernilai unik di tabel m_user kolom username kecuali untuk user dengan id yang sedang diedit
-            'username' => 'required|string|min:3|unique:m_user,username,'.$id.',user_id',
+            'username' => 'required|string|min:3|unique:m_user,username,' . $id . ',user_id',
             'nama'     => 'required|string|max:100', // nama harus diisi, string, maksimal 100 karakter
             'password' => 'nullable|min:5',          // password bisa diisi (minimal 5 karakter) dan bisa tidak diisi
             'level_id' => 'required|integer'         // level_id harus diisi dan berupa angka
@@ -314,11 +321,11 @@ class UserController extends Controller
             return redirect('/user')->with('error', 'Data user tidak ditemukan');
         }
 
-        try{
+        try {
             UserModel::destroy($id);    // Hapus data user
 
             return redirect('/user')->with('success', 'Data user berhasil dihapus');
-        }catch (\Illuminate\Database\QueryException $e){
+        } catch (\Illuminate\Database\QueryException $e) {
             // Jika terjadi error ketika menghapus data, redirect kembali ke halaman dengan membawa pesan error
             return redirect('/user')->with('error', 'Data user gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
         }
