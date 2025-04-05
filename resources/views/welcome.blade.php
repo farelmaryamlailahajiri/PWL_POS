9961412cf4e941de9bb8ac0353dc24507174362c
farelmaryamlailahajiri <farelhajiri295@gmail.com>
2025-04-05 20:38:26 +0700

    layouting jb5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,13 @@
-@extends('layout.app')  {{--memanggil folder layout,file app --}}
-
-{{-- customize layout sections --}}
-@section('subtitle', 'Welcome') {{-- Menetapkan subtitle halaman menjadi "Welcome" --}}
-@section('content_header_title', 'Home') {{-- Menetapkan judul header halaman menjadi "Home" --}}
-@section('content_header_subtitle', 'Welcome') {{-- Menetapkan subtitle header halaman menjadi "Welcome" --}}
-
-{{-- content body: main page content --}}
-@section('content_body')
-    <p>Welcome to this beautiful admin panel</p> {{-- Menampilkan teks sambutan pada panel admin --}}
-@endsection
-
-{{-- push extra css --}}
-@push('css')
-    {{-- add here extra stylesheet --}}
-    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
-@endpush
-
-{{-- push extra scripts --}}
-@push('js')
-    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!");</script>   {{-- Menampilkan pesan di console browser untuk debugging --}}
-@endpush
+@extends('layouts.template')
+ 
+ @section('content')
+ <div class="card">
+     <div class="card-header">
+         <h3 class="card-title">Halo, apakabar!!!</h3>
+         <div class="card-tools"></div>
+     </div>
+     <div class="card-body">
+         Selamat datang semua, ini adalah halaman utama dari aplikasi ini.
+     </div>
+ </div>
+ @endsection
