844b6b9c203a908f6bd84356d10d57c2cbebb835
farelmaryamlailahajiri <farelhajiri295@gmail.com>
2025-04-06 12:24:39 +0700

    prak3 jb 6 ajax delete data user

--- routes/web.php
+++ routes/web.php
@@ -46,6 +46,8 @@ Route::group(['prefix' => 'user'], function () {
     Route::put('/{id}', [UserController::class, 'update']);         // Menyimpan perubahan data user
     Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);     // Menampilkan halaman form edit user Ajax
     Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);  // Menyimpan perubahan data user Ajax
+    Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']); //untuk tampilkan form confirm delete user ajax
+    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']); //untuk delete user ajax
     Route::delete('/{id}', [UserController::class, 'destroy']);     // Menghapus data user
 });
 
