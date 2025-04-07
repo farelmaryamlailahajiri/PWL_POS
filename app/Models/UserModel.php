aca0f7ef309e4013bc11a469e60adbf0778572d7
farelmaryamlailahajiri <farelhajiri295@gmail.com>
2025-04-07 11:20:01 +0700

    prak1 jb7 authentication

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,16 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable; //implementasi class authenticatable
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
 
     protected $table = 'm_user'; //mendefinisikan nama tabel yg digunakan oleh model ini
     protected $primaryKey = 'user_id'; //mendefinisikan nama primary key yg digunakan
-
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
-    // protected $fillable = ['level_id', 'username', 'nama'];
+
+    protected $hidden = ['password']; // jangan di tampilkan saat select
+    protected $casts = ['password' => 'hashed']; //casting password agar auto di hash
+    //relasi ke tabel level
     public function level(): BelongsTo {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -64,6 +64,12 @@
                     <p>Supplier Barang</p>
                 </a>
             </li>
+            <li class="nav-item">
+                <a href="{{ url('/logout') }}" class="nav-link bg-danger text-white">
+                    <i class="nav-icon fas fa-sign-out-alt"></i>
+                    <p>Logout</p>
+                </a>
+            </li>
         </ul>
     </nav>
 </div>
